27a7d83c3a416a8bddc0c51117d5072238a6dcd1
Taphagol <tc1ndao@yahoo.fr>
2020-06-10 01:07:22 -0400

    Registering services in razor pages project

--- Demo-RazorPages/Pages/Employees/Index.cshtml.cs
+++ Demo-RazorPages/Pages/Employees/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Demo_RazorPages.Models;
+using Demo_RazorPages.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,17 @@ namespace Demo_RazorPages.Pages.Employees
 {
     public class IndexModel : PageModel
     {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public IEnumerable<Employee> Employees { get; set; }
+        public IndexModel(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+        
         public void OnGet()
         {
-
+            Employees = employeeRepository.GetAllEmployees();
         }
     }
 }
