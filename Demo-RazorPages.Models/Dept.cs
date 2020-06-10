4633003e9dcb1e71627b8324915ce01e7993ac28
Taphagol <tc1ndao@yahoo.fr>
2020-06-10 00:35:59 -0400

    Creating reusable models project

---
+++ Demo-RazorPages.Models/Dept.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_RazorPages.Models
+{
+    public enum Dept
+    {
+        None,
+        HR,
+        IT,
+        Payroll
+    }
+}
---
+++ Demo-RazorPages/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo_RazorPages.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
