67126e1d42aec62fa872d34f31762f28bce2695c
Taphagol <tc1ndao@yahoo.fr>
2020-07-09 19:39:33 -0400

    Retrieving List in Razor pages

--- Demo-RazorPages.Services/MockEmployeeRepository.cs
+++ Demo-RazorPages.Services/MockEmployeeRepository.cs
@@ -12,12 +12,12 @@ namespace Demo_RazorPages.Services
         {
             _employeeList = new List<Employee>()
             {
-                new Employee() { Id = 1, Name = "Mary", Department = Dept.HR,
-                    Email = "mary@ndawene.com", PhotoPath="mary.png" },
-                new Employee() { Id = 2, Name = "John", Department = Dept.IT,
-                    Email = "john@ndawene.com", PhotoPath="john.png" },
-                new Employee() { Id = 3, Name = "Sara", Department = Dept.IT,
-                    Email = "sara@ndawene.com", PhotoPath="sara.png" },
+                new Employee() { Id = 1, Name = "Jean", Department = Dept.HR,
+                    Email = "jean@ndawene.com", PhotoPath="jean.png" },
+                new Employee() { Id = 2, Name = "Nicolas", Department = Dept.IT,
+                    Email = "nicolas@ndawene.com", PhotoPath="nicolas.jpg" },
+                new Employee() { Id = 3, Name = "Sophie", Department = Dept.IT,
+                    Email = "sophie@ndawene.com", PhotoPath="sophie.jpeg" },
                 new Employee() { Id = 4, Name = "David", Department = Dept.Payroll,
                     Email = "david@ndawene.com" },
             };
